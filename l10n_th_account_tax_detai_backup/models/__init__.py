ffb7a4dc1568df022b6b3c3a2a73e56a368d0723
Kitti U <kittiu@gmail.com>
2018-03-14 01:52:58 +0700

    Remove unused addons

--- l10n_th_account_tax_detai_backup/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import account_tax_detail
-from . import account_invoice
-from . import account_voucher
-from . import account
-from . import account_config_settings
-from . import res_company
