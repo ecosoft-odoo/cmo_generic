d9a18324a6393139e3aeddde2c35e3f0ce338a5f
Saran L <saranp502@hotmail.com>
2019-03-06 13:23:35 +0700

    [IMP] cancel reverse bank_account_transfer

--- account_cancel_reversal/models/__init__.py
+++ account_cancel_reversal/models/__init__.py
@@ -4,4 +4,5 @@ from . import account_voucher
 from . import account
 from . import account_bank_receipt
 from . import account_bank_payment
+from . import bank_account_transfer
 from . import hr_salary
